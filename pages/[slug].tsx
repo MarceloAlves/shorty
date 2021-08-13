4350fe71012aa2ee622d9ae0741c4ac4b668a473
Marcelo Alves <selfmadecelo@gmail.com>
2021-08-12 17:53:01 -0700

    feat: working redirect

---
+++ pages/[slug].tsx
@@ -0,0 +1,34 @@
+import { GetServerSideProps } from 'next'
+import { getLinkBySlug } from '@services/db'
+
+/**
+ * This page exists solely to redirect to either a link or a 404.
+ * If everything fails, a 500 error is shown.
+ * User _should_ never see this page.
+ */
+
+export default function Slug() {
+  return <div />
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  try {
+    const link = await getLinkBySlug(query.slug as string)
+
+    return {
+      props: {},
+      redirect: {
+        status: link ? 301 : 404,
+        destination: link ? link.url : '/404',
+      },
+    }
+  } catch (error) {
+    return {
+      props: {},
+      redirect: {
+        statusCode: 500,
+        destination: '/500',
+      },
+    }
+  }
+}
