cbfba3b5fe38640174578455a28ec1cc75485bf5
Marcelo Alves <selfmadecelo@gmail.com>
2021-08-14 20:51:27 -0700

    fix: api routes in vercel

--- pages/api/link.ts
+++ pages/api/link.ts
@@ -9,30 +9,37 @@ interface ErrorResponse {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Link | ErrorResponse>) {
-  if (req.method === 'POST') {
-    try {
-      const linkData = {
-        ...req.body,
-        slug: await generateSlug(),
-      }
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
 
-      const result = await DBService.createLink(linkData)
+  let result: Link
 
-      res.status(201).json(result)
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        // Duplicate slug
-        // https://www.prisma.io/docs/reference/api-reference/error-reference/#p2002
-        if (error.code === 'P2002') {
-          res.status(409).json({
-            code: 409,
-            message: 'Slug already exists',
-          })
-        }
+  try {
+    const slug = await generateSlug()
+
+    const linkData = {
+      ...req.body,
+      slug,
+    }
+
+    result = await DBService.createLink(linkData)
+
+    if (!result) res.status(404).end('Not Found')
+
+    res.status(201).json(result)
+  } catch (error) {
+    console.log(`🚀 ~ file: link.ts ~ line 36 ~ handler ~ error`, error)
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // Duplicate slug
+      // https://www.prisma.io/docs/reference/api-reference/error-reference/#p2002
+      if (error.code === 'P2002') {
+        res.status(409).json({
+          code: 409,
+          message: 'Slug already exists',
+        })
       }
     }
   }
-
-  // 405: Method Not Allowed
-  res.status(405).end()
 }
