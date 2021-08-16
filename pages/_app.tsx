4e468ea52aa2673f3b581abd626b87ce1e128a68
Marcelo Alves <selfmadecelo@gmail.com>
2021-08-15 19:17:05 -0700

    chore: cleanup deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Hydrate } from 'react-query/hydration'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
-import { useState } from 'react'
 import { AppProvider } from '@providers/app'
 import ColorSchemeToggle from '@components/ColorSchemeToggle'
 
@@ -12,17 +9,11 @@ if (typeof Cypress !== 'undefined') {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient())
-
   return (
     <AppProvider>
       <ChakraProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={pageProps.dehydratedState}>
-            <ColorSchemeToggle />
-            <Component {...pageProps} />
-          </Hydrate>
-        </QueryClientProvider>
+        <ColorSchemeToggle />
+        <Component {...pageProps} />
       </ChakraProvider>
     </AppProvider>
   )
